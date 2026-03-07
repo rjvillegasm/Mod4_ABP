31210e8a1ad96d83e787a80d42881ba321f244cd
roberto <r.villegas@outlook.com>
2026-03-07 13:45:12 -0300

    implementa modulo cliente.py

--- modulos/Cliente.py
+++ modulos/Cliente.py
@@ -1,16 +1,49 @@
 class Cliente:
-    def __init__(self):
-        pass    
+    def __init__(self, cliente_id, nombre, email):
+        
+        # Validamos que el ID sea entero y postivo
+        if not isinstance(cliente_id, int):
+            raise TypeError("El ID debe ser un entero")
+        if cliente_id<=0:
+            raise ValueError("El ID deber ser un valor posistivo")
+        
+        # Validamos que el nombre sea un string y no esté vacío
+        if not isinstance( nombre, str):
+            raise TypeError("El nombre debe ser una cadena del tipo string")
+        if not nombre.strip():
+            raise ValueError("El nombre no puede estar vacío")
+        
+        # Validamos el email, debe ser una cadena y contener '@'
+        if not isinstance (email, str):
+            raise TypeError("El email debe ser una cadena")
+        if '@' not in email:
+            raise ValueError("Email no válido")        
+        
+        # Instanciamos atributos privados
+        self.__id= cliente_id
+        self.__email=email
+        self.__nombre=nombre
     
+    # Función para obtener ID
     def get_id(self):
-        pass
+        return self.__id
     
+    # Función para obtener nombre
     def get_nombre(self):
-        pass
+        return self.__nombre
     
-    def set_nombre(self):
-        pass
+    # Función para establecer nombre
+    # Valida que sea una cadena y no esté vacío
+    def set_nombre(self, nombre):
+        if not isinstance(nombre, str):
+            raise TypeError("El nombre debe ser una cadena")
+        if not nombre.strip():
+            raise ValueError("El nombre no puede estar vacío")
+        
+        self.__nombre=nombre
     
+    # Función para calcular descuento
+    # Será sobrescrita en las clases hijas
     def calcular_descuento(self):
-        pass
+        return 0
     
