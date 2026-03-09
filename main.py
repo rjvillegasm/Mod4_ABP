d7a40e35d5bc15a9c5cd71b2b211cd07828439b8
roberto <r.villegas@outlook.com>
2026-03-09 17:16:34 -0300

    agrega metodo agregar_cliente en la interfaz

--- main.py
+++ main.py
@@ -5,35 +5,104 @@ from modulos.tipos_cliente import ClienteRegular, ClientePremium, ClienteCorpora
 import tkinter as tk
 from tkinter import messagebox, ttk
 
-def main():
-    gestor=GestorClientes()
-    
-    try:
-    
-        cliente_uno= ClienteRegular(1, "Ana", "ana@gmail.com",100)
-        cliente_dos= ClientePremium(2, "luis", "luis@gmail.com", "Vip")
-        cliente_tres=ClienteCorporativo(3, "Tesla","empresat@gmail.com", "Tesla Corp. S.A.")
+# Métodos
 
-        gestor.agregar(cliente_uno)
-        gestor.agregar(cliente_dos)
-        gestor.agregar(cliente_tres)
-        
-        print("\n-- Lista de clientes--")
-        gestor.listar()
-        
-        print("\n-- Eliminando cliente--")
-        gestor.eliminar(1)
-        
-        print("\n-- Lista actualizada--")
-        gestor.listar()
-        
-        print("\n-- buscar cliente--")
-        gestor.obtener_cliente_individual(3)
-        #gestor.obtener_cliente_individual(-1)
-        gestor.obtener_cliente_individual(0)
-                
-    except(TypeError, ValueError) as e:
-        print(f"Error: {e}")
+def agregar_cliente():
+    id= entrada_id.get()
+    nombre = entrada_nombre.get()
+    email = entrada_email.get()
+    tipo = tipo_var.get()
+    puntos=entrada_puntos.get()
+    membresia=entrada_membresia.get()
+    empresa=entrada_empresa.get()     
+    
+    if tipo == "Regular":
+        cliente = ClienteRegular(id, nombre, email, puntos)
+    elif tipo == "Premium":
+        cliente = ClientePremium(id, nombre, email, membresia)
+    elif tipo =="Corporativo":  
+        cliente = ClienteCorporativo(id, nombre, email, empresa)
+    
+    gestor.agregar(cliente)
+    
+    entrada_id.delete(0,tk.END)
+    entrada_nombre.delete(0, tk.END)
+    entrada_email.delete(0, tk.END)
+    entrada_puntos.delete(0, tk.END)
+    entrada_membresia.delete(0, tk.END)
+    entrada_empresa.delete(0, tk.END)
 
+def seleccionar_tipo(tipo_cliente):
+    tipo = tipo_var.get()
+        
+    entrada_puntos.config(state="disabled")
+    entrada_membresia.config(state="disabled")
+    entrada_empresa.config(state="disabled")
+    
+    
+    if tipo == "Regular":
+        entrada_puntos.config(state="normal")
+    elif tipo == "Premium":
+        entrada_membresia.config(state="normal")
+    elif tipo == "Corporativo":
+        entrada_empresa.config(state="normal")
+
+# Inicialización
 if __name__=="__main__":
-    main()
+    
+    
+    
+    # Ventana principal
+    ventana=tk.Tk()
+    ventana.title("Gestor de clientes")
+    ventana.geometry("500x700")
+    
+    # Variables
+    gestor=GestorClientes()
+    tipo_var=tk.StringVar()
+    
+    #Widgets
+    
+    # Widget combobox para seleccionar tipo de clienete
+    tipo_combo = ttk.Combobox(ventana,
+                                textvariable=tipo_var, 
+                                values=["Regular", "Premium", "Corporativo"])
+    tipo_combo.bind("<<ComboboxSelected>>", seleccionar_tipo)
+    tipo_combo.pack()
+    
+    # Widget para agregar clientes
+    tk.Label(ventana, text="ID:").pack()
+    entrada_id = tk.Entry(ventana)
+    entrada_id.pack()
+    
+    tk.Label(ventana, text="Nombre:").pack()
+    entrada_nombre = tk.Entry(ventana)
+    entrada_nombre.pack()
+    
+    tk.Label(ventana, text="Email:").pack()
+    entrada_email = tk.Entry(ventana)
+    entrada_email.pack()
+    
+    tk.Label(ventana, text="Puntos:").pack()
+    entrada_puntos = tk.Entry(ventana)
+    entrada_puntos.pack()
+    
+    tk.Label(ventana, text="Membresía:").pack()
+    entrada_membresia = tk.Entry(ventana)
+    entrada_membresia.pack()
+    
+    tk.Label(ventana, text="Empresa:").pack()
+    entrada_empresa = tk.Entry(ventana)
+    entrada_empresa.pack()
+    
+    seleccionar_tipo(None)
+    
+    
+    # Botones
+    tk.Button(ventana, text="Agregar Cliente", command=agregar_cliente).pack()
+    
+    
+    
+    
+    # Bucle de la app
+    ventana.mainloop()
